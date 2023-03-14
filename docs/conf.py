27300c5224110a34ea80f3d50972cc6123c4bf22
Kristian Georgiev <krisgrg@mit.edu>
2023-03-13 22:08:53 -0400

    update sphinx conf.py for github pages

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,8 @@ release = '0.1.0'
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.todo",
-    "sphinx.ext.viewcode"
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
