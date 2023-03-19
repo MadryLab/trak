5f250b51c3ae621d65082916aaf8526ada590b33
Andrew Ilyas <ailyas@mit.edu>
2023-03-18 22:27:33 -0400

    update

--- docs/plot.js
+++ docs/plot.js
@@ -1,4 +1,16 @@
-  const table1 = [
+function updateLegendPosition(layout) {
+    layout.legend = {
+        x: 0.5,
+        y: 1.2,
+        xanchor: 'center',
+        orientation: 'h',
+    };
+
+    Plotly.update('scatterplot-container1', {}, layout);
+    Plotly.update('scatterplot-container2', {}, layout);
+}
+
+const table1 = [
     { method: "TRAK", computationTime: 1, correlation: 0.058, error: 0.0039 },
     { method: "TRAK", computationTime: 5, correlation: 0.12, error: 0.0036 },
     { method: "TRAK", computationTime: 20, correlation: 0.27, error: 0.0042 },
@@ -14,88 +26,114 @@
     { method: "Emp. Influence [FZ20]", computationTime: 25000, correlation: 0.28, error: 0.0041 },
     { method: "IF-Arnoldi [SZV+22]", computationTime: 120, correlation: 0.020, error: 0.0048 },
     { method: "IF [KL17]", computationTime: 25003, correlation: 0.037, error: 0.021 },
-    { method: "Representation Dist.", computationTime: 50, correlation: 0.029, error: 0.0060 },
+    { method: "Representation Distance", computationTime: 50, correlation: 0.029, error: 0.0060 },
     { method: "GAS [HL22]", computationTime: 30, correlation: 0.047, error: 0.0072 },
     { method: "TracIn [PLS+20]", computationTime: 15, correlation: 0.056, error: 0.0073 },
     { method: "TracIn [PLS+20]", computationTime: 300, correlation: 0.055, error: 0.0070 },
-  ];
+];
 
-  const table2 = [
-    { method: "Datamodel [IPE+22]", computationTime: 43200.0, correlation: 0.178846, error: 0.033482 },
-    { method: "Datamodel [IPE+22]", computationTime: 85500.0, correlation: 0.258097, error: 0.033210 },
-    { method: "Datamodel [IPE+22]", computationTime: 175500.0, correlation: 0.344712, error: 0.031926 },
-    { method: "TracIn [PLS+20]", computationTime: 284, correlation: 0.072531, error: 0.025567 },
-    { method: "Emp. Influence [FZ20]", computationTime: 43200.0, correlation: 0.165813, error: 0.041610 },
-    { method: "Emp. Influence [FZ20]", computationTime: 85500.0, correlation: 0.202983, error: 0.046267 },
-    { method: "Emp. Influence [FZ20]", computationTime: 175500.0, correlation: 0.225179, error: 0.044004 },
-    { method: "TRAK", computationTime: 64, correlation: 0.178444, error: 0.005628 },
-    { method: "TRAK", computationTime: 640, correlation: 0.416264, error: 0.010370 },
-    { method: "TRAK", computationTime: 6400, correlation: 0.593538, error: 0.014348 },
-    { method: "IF [KL17]", computationTime: 18042.9, correlation: 0.113670, error: 0.043225 },
-    { method: "IF [KL17]", computationTime: 90214.5, correlation: 0.155935, error: 0.029792 },
-    { method: "Rep. Distance", computationTime: 90, correlation: 0.050549, error: 0.047340 },
-    { method: "Rep. Distance", computationTime: 180, correlation: 0.050256, error: 0.048500 },
-    { method: "GAS [HL22]", computationTime: 284.0, correlation: 0.077749, error: 0.028504 },
-  ];
+const table2 = [
+    { method: "Datamodel [IPE+22]", computationTime: 43200.0, correlation: 0.18, error: 0.033 },
+    { method: "Datamodel [IPE+22]", computationTime: 85500.0, correlation: 0.26, error: 0.033 },
+    { method: "Datamodel [IPE+22]", computationTime: 175500.0, correlation: 0.34, error: 0.032 },
+    { method: "TracIn [PLS+20]", computationTime: 284, correlation: 0.073, error: 0.026 },
+    { method: "Emp. Influence [FZ20]", computationTime: 43200.0, correlation: 0.17, error: 0.042 },
+    { method: "Emp. Influence [FZ20]", computationTime: 85500.0, correlation: 0.20, error: 0.046 },
+    { method: "Emp. Influence [FZ20]", computationTime: 175500.0, correlation: 0.23, error: 0.044 },
+    { method: "TRAK", computationTime: 64, correlation: 0.18, error: 0.0056 },
+    { method: "TRAK", computationTime: 640, correlation: 0.42, error: 0.010 },
+    { method: "TRAK", computationTime: 6400, correlation: 0.59, error: 0.014 },
+    { method: "IF [KL17]", computationTime: 18042.9, correlation: 0.11, error: 0.043 },
+    { method: "IF [KL17]", computationTime: 90214.5, correlation: 0.16, error: 0.030 },
+    { method: "Representation Distance", computationTime: 90, correlation: 0.051, error: 0.047 },
+    { method: "Representation Distance", computationTime: 180, correlation: 0.050, error: 0.049 },
+    { method: "GAS [HL22]", computationTime: 284.0, correlation: 0.078, error: 0.029 },
+];
 
 
-  const methods = [
+
+const methods = [
     "Datamodel [IPE+22]",
     "TracIn [PLS+20]",
     "Emp. Influence [FZ20]",
     "TRAK",
     "IF [KL17]",
-    "Rep. Distance",
+    "Representation Distance",
     "GAS [HL22]",
-  ];
-  
-  const colors = [
-    "rgba(255, 0, 0, 0.8)",
-    "rgba(0, 255, 0, 0.8)",
-    "rgba(0, 0, 255, 0.8)",
-    "rgba(255, 128, 0, 0.8)",
-    "rgba(128, 0, 255, 0.8)",
-    "rgba(0, 255, 255, 0.8)",
-    "rgba(255, 0, 255, 0.8)",
-  ];
+];
+
+const colors = [
+    '#FF6B6B', // "rgba(255, 0, 0, 0.8)",
+    '#F0E66B', // "rgba(0, 255, 0, 0.8)",
+    '#4EDEA4', // "rgba(0, 0, 255, 0.8)",
+    '#5AB5FF', // "rgba(255, 128, 0, 0.8)",
+    '#936BFF', // "rgba(128, 0, 255, 0.8)",
+    '#FF8ECF', // "rgba(0, 255, 255, 0.8)",
+    '#FFB86B' // "rgba(255, 0, 255, 0.8)",
+];
 
 
-  const scatterPlot = (data, id) => {
+const scatterPlot = (data, id) => {
     const traces = methods.map((method, index) => {
-  return {
-    x: data
-      .filter((entry) => entry.method === method)
-      .map((entry) => entry.computationTime),
-    y: data
-      .filter((entry) => entry.method === method)
-      .map((entry) => entry.correlation),
-    error_y: {
-      type: "data",
-      array: data
-        .filter((entry) => entry.method === method)
-        .map((entry) => entry.error),
-      visible: true,
-    },
-    mode: "markers",
-    type: "scatter",
-    name: method,
-    marker: { color: colors[index], size: 12 },
-  };
-});
+        return {
+            x: data
+                .filter((entry) => entry.method === method)
+                .map((entry) => entry.computationTime),
+            y: data
+                .filter((entry) => entry.method === method)
+                .map((entry) => entry.correlation),
+            error_y: {
+                type: "data",
+                array: data
+                    .filter((entry) => entry.method === method)
+                    .map((entry) => entry.error),
+                visible: true,
+            },
+            mode: "markers",
+            type: "scatter",
+            name: method,
+            marker: { color: colors[index], size: 12 }
+        };
+    });
 
-// Define chart layout
-// Log scale x axis
-const layout = {
-    title: "Scatterplot",
-    xaxis: { title: "Computation Time (mins)" , type: 'log'},
-    yaxis: { title: "Correlation" }
+    // Define chart layout
+    // Log scale x axis
+    const bgColor = 'rgb(33, 37, 41)';
+    const layout = {
+        plot_bgcolor: bgColor,
+        paper_bgcolor: bgColor,
+        font: { color: 'white' },
+        xaxis: { 
+            title: "Computation Time (mins)", 
+            type: 'log',
+            gridcolor: 'rgba(255, 255, 255, 0.2)',
+            zerolinecolor: 'rgba(255, 255, 255, 0.4)',
+            linecolor: 'rgba(255, 255, 255, 0.6)',
+        },
+        yaxis: { 
+            title: "Correlation", 
+            gridcolor: 'rgba(255, 255, 255, 0.2)',
+            zerolinecolor: 'rgba(255, 255, 255, 0.4)',
+            linecolor: 'rgba(255, 255, 255, 0.6)',
+        },
+        // legend: { orientation: "h" },
+        autosize: true
+    };
+
+    // Draw the chart
+    Plotly.newPlot(id, traces, layout, { responsive: true });
+    return layout;
 };
 
-// Draw the chart
-Plotly.newPlot(id, traces, layout);
-  };
-  
-  scatterPlot(table1, 'scatterplot-container1');
-  scatterPlot(table2, 'scatterplot-container2');
-  
-  
+// Update the legend position when the window is resized
+// window.addEventListener('resize', () => updateLegendPosition(layout));
+window.addEventListener('load', () => {
+    scatterPlot(table1, 'scatterplot-container1');
+    document.getElementById('scatterplot-container1');
+    // .on('plotly_ready', () => {
+        // updateLegendPosition(layout);
+        // Plotly.Plots.resize('scatterplot-container1');
+    // });
+    scatterPlot(table2, 'scatterplot-container2');
+    // updateLegendPosition(layout);
+});
