cd9490cef2230a8a9f09766f2bd2e088cf94c4fb
Kristian Georgiev <krisgrg@mit.edu>
2023-03-22 20:18:52 -0400

    some memory profiling tests

---
+++ tests/memory_profiling.py
@@ -0,0 +1,67 @@
+from tqdm import tqdm
+from pathlib import Path
+from pytorch_memlab import LineProfiler, MemReporter
+from trak import TRAKer
+import torch
+ch = torch
+
+from utils import construct_rn9, get_dataloader, eval_correlations
+from utils import download_cifar_checkpoints, download_cifar_betons
+
+
+def test_cifar_acc(serialize=False, dtype=ch.float32, batch_size=100, tmp_path='/tmp/trak_results/'):
+    device = 'cuda:0'
+    model = construct_rn9().to(memory_format=ch.channels_last).to(device)
+    model = model.eval()
+
+    BETONS_PATH = Path(tmp_path).joinpath('cifar_betons')
+    BETONS = download_cifar_betons(BETONS_PATH)
+
+    loader_train = get_dataloader(BETONS, batch_size=batch_size, split='train')
+    loader_val = get_dataloader(BETONS, batch_size=batch_size, split='val')
+
+    CKPT_PATH = Path(tmp_path).joinpath('cifar_ckpts')
+    ckpt_files = download_cifar_checkpoints(CKPT_PATH)
+    ckpts = [ch.load(ckpt, map_location='cpu') for ckpt in ckpt_files]
+
+    reporter = MemReporter()
+
+    traker = TRAKer(model=model,
+                    task='image_classification',
+                    proj_dim=1024,
+                    train_set_size=10_000,
+                    save_dir=tmp_path,
+                    device=device)
+
+    for model_id, ckpt in enumerate(ckpts):
+        i = 0
+
+        traker.load_checkpoint(checkpoint=ckpt, model_id=model_id)
+
+        for batch in tqdm(loader_train, desc='Computing TRAK embeddings...'):
+            traker.featurize(batch=batch, num_samples=len(batch[0]))
+            reporter.report()
+
+    traker.finalize_features()
+
+    if serialize:
+        del traker
+        traker = TRAKer(model=model,
+                        task='image_classification',
+                        projector=projector,
+                        proj_dim=1024,
+                        train_set_size=10_000,
+                        save_dir=tmp_path,
+                        device=device)
+
+    for model_id, ckpt in enumerate(ckpts):
+        traker.start_scoring_checkpoint(ckpt, model_id, num_targets=2_000)
+        for batch in tqdm(loader_val, desc='Scoring...'):
+            traker.score(batch=batch, num_samples=len(batch[0]))
+
+    scores = traker.finalize_scores().cpu()
+
+with LineProfiler(test_cifar_acc, TRAKer.featurize, TRAKer.load_checkpoint) as prof:
+    test_cifar_acc()
+
+prof.print_stats()
