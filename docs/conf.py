32862dbb6f91667b605c93a2f5de411bd3bf1e76
Kristian Georgiev <krisgrg@mit.edu>
2023-03-23 01:05:38 -0400

    update sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Kristian Georgiev'
 author = 'Kristian Georgiev'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
