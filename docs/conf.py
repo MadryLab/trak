821f02c22667e8a811f54e1b5065c0e6fc3292cc
Kristian Georgiev <krisgrg@mit.edu>
2023-03-23 16:29:20 -0400

    add quickstart to sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages"
+    "sphinx.ext.githubpages",
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
