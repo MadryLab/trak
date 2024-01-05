1febdc9e912bcc8c44e505eeae1509fc66b83b1d
Jiadong Guo <jaedon.guo@gmail.com>
2024-01-05 02:10:54 -0700

    fix finalize features floods CPU memory: remove log and load mapped gradients to device  (#59)
    * torch load gradients to device instead of CPU during finalize
    
    loading data directly on device instead of moving from CPU to GPU in score computation steps
    
    * Remove logging of raw tensors

--- trak/traker.py
+++ trak/traker.py
@@ -473,12 +473,10 @@ class TRAKer:
 
             self.saver.load_current_store(model_id)
 
-            g = ch.as_tensor(self.saver.current_store["grads"])
+            g = ch.as_tensor(self.saver.current_store["grads"], device=self.device)
             xtx = self.score_computer.get_xtx(g)
-            self.logger.debug(f"XTX is {xtx}")
 
             features = self.score_computer.get_x_xtx_inv(g, xtx)
-            self.logger.debug(f"Features are {features}")
             self.saver.current_store["features"][:] = features.to(self.dtype).cpu()
             if del_grads:
                 self.saver.del_grads(model_id)
